937a54e17424a3a35f7112f8f712a2f308bb0132
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2024-01-27 17:36:04 +0200

    customizing radix ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import NavBar from "@/app/NavBar";
-import { Theme } from "@radix-ui/themes"
-import './globals.css';
+import {Theme, ThemePanel} from "@radix-ui/themes";
+
 import '@radix-ui/themes/styles.css';
+import './theme-config.css';
+import './globals.css';
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  variable: "--font-inter"
+})
 
 export const metadata: Metadata = {
   title: 'Issue Tracker',
   description: 'create next app',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme appearance="light" accentColor="violet">
           <NavBar/>
 
           <main className='p-5'>
